4d45775b140744c6a8a818642bf88ae9bf82a0c8
“mohammadfarhadfard” <mohammadfarhadfard@gmail.com>
2023-11-29 02:12:02 +0330

    timers change and add trx,eth

--- axios.js
+++ axios.js
@@ -12,18 +12,48 @@ setInterval(()=>{
 
   axios.get(kucoinApi)
     .then( function(response) {
-         price = response.data.data.price
-        console.log("BTC:" ,price);
+         price1 = response.data.data.price
+        // console.log("BTC:" ,price);
     })
     .catch(error => {
      console.log("err: " +error)
     }) 
-},2000)
+},2500)
+
+
+setInterval(()=>{
+  
+ let kucoinApi="https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=TRX-USDT"
+
+  axios.get(kucoinApi)
+    .then( function(response) {
+         price2 = response.data.data.price
+        // console.log("TRX:" ,price);
+    })
+    .catch(error => {
+     console.log("err: " +error)
+    }) 
+},2500)
+
+
+setInterval(()=>{
+  
+ let kucoinApi="https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=ETH-USDT"
+
+  axios.get(kucoinApi)
+    .then( function(response) {
+         price3 = response.data.data.price
+        // console.log("ETH:" ,price);
+    })
+    .catch(error => {
+     console.log("err: " +error)
+    }) 
+},2500)
 
 
  setInterval(()=>{
 
-   let path = `https://api.telegram.org/bot6775787608:AAF2d7l05TtGQTXL12dTyhPyKjFqr9fuIvc/sendMessage\?chat_id\=-1002136043768\&text\= BTC :${price}`
+   let path = `https://api.telegram.org/bot6775787608:AAF2d7l05TtGQTXL12dTyhPyKjFqr9fuIvc/sendMessage\?chat_id\=-1002136043768\&text\= BTC :${price1},TRX :${price2},ETH :${price3}`
 
    axios.get(path)
      .then( function(response) {
@@ -31,7 +61,7 @@ setInterval(()=>{
      .catch(error => {
       console.log("err: " +error)
      })
- },3500)
+ },20000)
 
 
 
